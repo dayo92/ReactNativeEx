98f8be6589f500d39abf4094573604a14e273422
Daniel Ataieryo <daniel.ataiyero@jdplc.com>
2021-09-30 00:21:09 +0100

    flexbox

--- App.js
+++ App.js
@@ -1,45 +1,36 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableHighlight, Alert, SafeAreaView, TouchableNativeFeedback,
-         Button, StatusBar, Platform, Dimensions} from 'react-native';
-
-import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks';
+import { View} from 'react-native';
 
 export default function App() {
 
-  // console.log("App executed");
-  //to see with and height of phone
-  // console.log(Dimensions.get('screen'));
-  //Dimensions update when phone is on portrait or landscape
-  // console.log(useDimensions(),"test")
-  //returs object with properties true/false for currrent position of portrait and landscape
-  // console.log(useDeviceOrientation());
-
-  const {landscape} = useDeviceOrientation();
   
   return (
 
-    <SafeAreaView style={styles.container}>
-        <View style={{
-          backgroundColor:"dodgerblue",
-          width:'100%',
-          //if phone is on landscape chage height to 100% else 30%
-          height: landscape ? '100%' : '30%'
+    <View 
+    //container
+      style={{
+        backgroundColor: '#fff',
+        flex:1,
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center',
+        // alignContent:'center',
+        // flexWrap:'wrap'
+    }}>
+        <View style={{backgroundColor:"dodgerblue", 
+        // width:100, 
+        height:100, 
+        flexBasis:100 //with or height
         }}></View>
-    </SafeAreaView>
+        <View style={{backgroundColor:"gold", width:100, height:100 , top:20, left:20, position:'absolute'}}></View>
+        <View style={{backgroundColor:"tomato", width:100, height:100 }}></View>
+        {/* <View style={{backgroundColor:"gray", width:100, height:100 }}></View> */}
+        {/* <View style={{backgroundColor:"greenyellow", width:100, height:100 }}></View> */}
 
+    </View>
    
   );
 }
 
-const containerStyle = {backgroundColor:"orange"}
-;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'green',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-    // alignItems: 'center',
-    // justifyContent: 'center'
-  },
-});
+
